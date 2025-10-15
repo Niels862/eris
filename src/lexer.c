980d8153dabf5cc0d84b9dba0ee9fbb98513f268
Niels862 <vschagenniels@gmail.com>
2025-10-15 19:46:04 +0200

    lexing keywords, operators & separators

--- src/lexer.c
+++ src/lexer.c
@@ -2,6 +2,22 @@
 #include "token.h"
 #include <ctype.h>
 #include <stdbool.h>
+#include <assert.h>
+
+static ctk_fixed_lexeme_t eris_keywords[] = {
+    ERIS_TOKENS_KEYWORD(ERIS_X_EXPAND_FIXED),
+    { 0 }
+};
+
+static ctk_fixed_lexeme_t eris_separators[] = {
+    ERIS_TOKENS_SEPARATOR(ERIS_X_EXPAND_FIXED),
+    { 0 }
+};
+
+static ctk_fixed_lexeme_t eris_operators[] = {
+    ERIS_TOKENS_OPERATOR(ERIS_X_EXPAND_FIXED),
+    { 0 }
+};
 
 static void eris_emit(ctk_lexer_t *lexer, ctk_tokenlist_t *toks, 
                       eris_tokenkind_t kind) {
@@ -21,6 +37,46 @@ static bool eris_is_identifier_continue(ctk_lexer_t *lexer) {
     return isalnum(c) || c == '_';
 }
 
+static bool eris_is_separator(ctk_lexer_t *lexer) {
+    switch (lexer->curr) {
+        case '{':
+        case '}':
+        case '(':
+        case ')':
+        case '[':
+        case ']':
+        case ';':
+        case ':':
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+static bool eris_is_operator(ctk_lexer_t *lexer) {
+    switch (lexer->curr) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+        case '<':
+        case '>':
+        case '=':
+        case '~':
+        case '|':
+        case '&':
+        case '?':
+        case '!':
+        case '@':
+            return true;
+
+        default:
+            return false;
+    }
+}
+
 static bool eris_is_whitespace(ctk_lexer_t *lexer) {
     uint32_t c = lexer->curr;
     return c == ' ' || c == '\t' || c == '\n' || c == '\r';
@@ -35,29 +91,80 @@ static bool eris_is_comment_continue(ctk_lexer_t *lexer) {
     return isprint(c) && c != '\n';
 }
 
+static void eris_lex_identifier(ctk_lexer_t *lexer, ctk_tokenlist_t *toks) {
+    do {
+        ctk_lexer_advance(lexer);
+    } while (eris_is_identifier_continue(lexer));
+
+    eris_tokenkind_t kind = ctk_lexer_lookup(lexer, eris_keywords);
+    if (kind == ERIS_TOKEN_NULL) {
+        kind = ERIS_TOKEN_IDENTIFIER;
+    }
+
+    eris_emit(lexer, toks, kind);
+}
+
+static void eris_lex_separator(ctk_lexer_t *lexer, ctk_tokenlist_t *toks) {
+    ctk_lexer_advance(lexer);
+
+    eris_tokenkind_t kind = ctk_lexer_lookup(lexer, eris_separators);
+    assert(kind != ERIS_TOKEN_NULL);
+
+    eris_emit(lexer, toks, kind);
+}
+
+static void eris_lex_operator(ctk_lexer_t *lexer, ctk_tokenlist_t *toks) {
+    ctk_lexer_state_t state;
+    eris_tokenkind_t kind = ERIS_TOKEN_UNRECOGNIZED;
+
+    do {
+        ctk_lexer_advance(lexer);
+
+        eris_tokenkind_t lkind = ctk_lexer_lookup(lexer, eris_operators);
+        if (lkind != ERIS_TOKEN_NULL) {
+            ctk_lexer_save_state(lexer, &state);
+            kind = lkind;
+        }
+    } while (eris_is_operator(lexer));
+
+    if (kind != ERIS_TOKEN_UNRECOGNIZED) {
+        ctk_lexer_restore_state(lexer, &state);
+    }
+    
+    eris_emit(lexer, toks, kind);
+}
+
+static void eris_lex_whitespace(ctk_lexer_t *lexer) {
+    do {
+        ctk_lexer_advance(lexer);
+    } while (eris_is_whitespace(lexer));
+
+    ctk_lexer_discard(lexer);
+}
+
+static void eris_lex_comment(ctk_lexer_t *lexer) {
+    do {
+        ctk_lexer_advance(lexer);
+    } while (eris_is_comment_continue(lexer));
+
+    ctk_lexer_discard(lexer);
+}
+
 void eris_lex(ctk_textsrc_t *ts, ctk_tokenlist_t *toks) {
     ctk_lexer_t lexer;
     ctk_lexer_init(&lexer, ts, ctk_decode_raw);
 
     do {
         if (eris_is_identifier_start(&lexer)) {
-            do {
-                ctk_lexer_advance(&lexer);
-            } while (eris_is_identifier_continue(&lexer));
-
-            eris_emit(&lexer, toks, ERIS_TOKEN_IDENTIFIER);
+            eris_lex_identifier(&lexer, toks);
+        } else if (eris_is_separator(&lexer)) {
+            eris_lex_separator(&lexer, toks);
+        } else if (eris_is_operator(&lexer)) {
+            eris_lex_operator(&lexer, toks);
         } else if (eris_is_whitespace(&lexer)) {
-            do {
-                ctk_lexer_advance(&lexer);
-            } while (eris_is_whitespace(&lexer));
-
-            ctk_lexer_discard(&lexer);
+            eris_lex_whitespace(&lexer);
         } else if (eris_is_comment_start(&lexer)) {
-            do {
-                ctk_lexer_advance(&lexer);
-            } while (eris_is_comment_continue(&lexer));
-
-            ctk_lexer_discard(&lexer);
+            eris_lex_comment(&lexer);
         } else {
             ctk_lexer_advance(&lexer);
             eris_emit(&lexer, toks, ERIS_TOKEN_UNRECOGNIZED);
--- src/token.h
+++ src/token.h
@@ -14,6 +14,39 @@
         X(IDENTIFIER, ""), \
         X(INTLIT, "") \
 
+#define ERIS_TOKENS_KEYWORD(X) \
+        X(FUNCTION, "function"), \
+        X(RETURN, "return"), \
+        X(IF, "if"), \
+        X(ELSE, "else"), \
+        X(WHILE, "while")
+
+#define ERIS_TOKENS_OPERATOR(X) \
+        X(ARROW, "->"), \
+        X(PLUS, "+"), \
+        X(MINUS, "-"), \
+        X(ASTERISK, "*"), \
+        X(SLASH, "/"), \
+        X(DSLASH, "//"), \
+        X(EQUAL, "="), \
+        X(DEQUAL, "=="), \
+        X(LT, "<"), \
+        X(GT, ">"), \
+        X(LTEQ, "<="), \
+        X(GTEQ, ">="), \
+        X(EXCL, "!"), \
+        X(EQCLEQ, "!=")
+
+#define ERIS_TOKENS_SEPARATOR(X) \
+        X(LBRACE, "{"), \
+        X(RBRACE, "}"), \
+        X(LPAREN, "("), \
+        X(RPAREN, ")"), \
+        X(LBRACK, "["), \
+        X(RBRACK, "]"), \
+        X(SEMICOLON, ";"), \
+        X(COLON, ":")
+
 #define ERIS_TOKENS_OTHER(X) \
         X(UNRECOGNIZED, ""), \
         X(EOF, "")
@@ -21,10 +54,14 @@
 #define ERIS_TOKENS(X) \
         ERIS_TOKENS_NULL(X), \
         ERIS_TOKENS_LITERAL(X), \
+        ERIS_TOKENS_KEYWORD(X), \
+        ERIS_TOKENS_OPERATOR(X), \
+        ERIS_TOKENS_SEPARATOR(X), \
         ERIS_TOKENS_OTHER(X)
 
 #define ERIS_X_EXPAND_ENUM(e, s) ERIS_TOKEN_##e
 #define ERIS_X_EXPAND_NAME(e, s) [ERIS_TOKEN_##e] = #e
+#define ERIS_X_EXPAND_FIXED(e, s) { s, ERIS_TOKEN_##e }
 
 typedef enum {
     ERIS_TOKENS(ERIS_X_EXPAND_ENUM)
