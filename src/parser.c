36f9d103ea2e74e11ea5edea90a644da4e215d3b
Niels862 <vschagenniels@gmail.com>
2025-11-02 23:00:15 +0100

    added errors with synthetic after tokens

--- src/parser.c
+++ src/parser.c
@@ -4,20 +4,44 @@
 #include "ctk/text-context-writer.h"
 #include <assert.h>
 
+static ctk_token_t eris_synthetic_after_token(ctk_token_t *tok) {
+    ctk_token_t *before = tok - 1;
+
+    // BEFORE is synthetic token (STARTSOURCE) -> give up
+    if (before->src->name == NULL) {
+        return *tok;
+    }
+
+    ctk_token_t after = *before;
+
+    // FIXME: needs to account for encoding and newlines
+    after.pos.col += after.lexeme.end - after.lexeme.start;
+    after.lexeme.start = after.lexeme.end;
+
+    return after;
+}
+
 static void eris_parser_expect_error(ctk_token_t *got, 
                                      int expected, char const *msg) {
     assert(got != NULL); // May not be called on synthetic NONE token
     assert(got->kind != 0);
     assert(expected != 0 || msg != NULL);
 
+    ctk_token_t tok;
+    if (got->kind == ERIS_TOKEN_EOF || expected == ERIS_TOKEN_SEMICOLON) {
+        tok = eris_synthetic_after_token(got);
+    } else {
+        tok = *got;
+    }
+
     ctk_textctx_writer_t writer = {
         .style = &eris_context_style,
-        .focus = got,
+        .focus = &tok,
     };
     
     fprintf(stderr, "%s:%d:%d: " CTK_ANSI_FG_BRIGHT(CTK_ANSI_RED) 
             "error:" CTK_ANSI_RESET, 
-            got->src->name, got->pos.line, got->pos.col);
+            tok.src->name, tok.pos.line, tok.pos.col);
     if (msg != NULL) {
         fprintf(stderr, " %s", msg);
     } else {
