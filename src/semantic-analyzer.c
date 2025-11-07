40369c129303ed64073ca183f792b105246ca60d
Niels862 <vschagenniels@gmail.com>
2025-11-07 16:12:12 +0100

    semantic analysis stub

---
+++ src/semantic-analyzer.c
@@ -0,0 +1,15 @@
+#include "semantic-analyzer.h"
+
+typedef struct eris_analyzer_t eris_analyzer_t;
+
+
+
+typedef void(*eris_analyzer_visit_t)(eris_analyzer_t *an, void *node);
+
+struct eris_analyzer_t {
+    eris_analyzer_visit_t *visitors;
+};
+
+void eris_analyze_semantics(eris_node_source_t *root) {
+    (void)root;
+}
---
+++ src/semantic-analyzer.h
@@ -0,0 +1,8 @@
+#ifndef ERIS_SEMANTIC_ANALYSIS_H
+#define ERIS_SEMANTIC_ANALYSIS_H
+
+#include "ast.h"
+
+void eris_analyze_semantics(eris_node_source_t *root);
+
+#endif
